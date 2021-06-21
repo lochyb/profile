c59e8259ba2d267c4da7ac0618c127f2f372ac74
Lochlan Bernard <lochlanbernard@gmail.com>
2021-06-21 20:58:56 +0900

    update

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -4,7 +4,7 @@ export const Info = () => {
   return (
     <div className='info'>
       <div className='info-wrapper'>
-        <img src='../images/profilePic.jpg' alt='profile'></img>
+        <img src='./images/profilePic.jpg' alt='profile'></img>
         <div className='text-wrapper'>
           <h3>Lochlan Bernard</h3>
           <h4>Aspiring Web Dev</h4>
