5ce6776c0a0032a8656a35cbe291779692b772c8
Lochlan Bernard <lbernard@nzigen.com>
2024-04-08 18:33:03 +0900

    Update sand-v2 with md explanation.

--- public/js/classes/GridV2.js
+++ public/js/classes/GridV2.js
@@ -5,121 +5,167 @@ class GridV2 {
         this.rows = height / particleWidth;
         this.particleWidth = particleWidth;
 
-        this.state = new Array(this.columns * this.rows).fill(new Empty);
-        this.nextState = new Array(this.columns * this.rows).fill(new Empty);
-        this.gravityState = new Array(this.columns * this.rows).fill(1);
-    }
-
-    swap(indexA, indexB) {
-        const temp = this.state[indexA];
-        this.state[indexA] = this.state[indexB];
-        this.state[indexB] = temp;
-    }
-
-    reset() {
-        this.state = new Array(this.columns * this.rows).fill(new Empty);
-    }
-
-    resetNext() {
+        this.state = this.generateInitialState();
         this.nextState = new Array(this.columns * this.rows).fill(new Empty);
     }
 
-    setStatePoint(column, row) {
-        this.state[(row * this.columns) + column] = new Sand;
+    findNextBelowIndex(index) {
+        const velocity = this.getParticleVelocity(index);
+        let returnIndex = index;
+
+        for (let vel = velocity; vel >= 0; vel--) {
+            const belowIndex = index + (vel * this.columns);
+
+            if (!this.isInbounds(belowIndex)) {
+                continue;
+            }
+
+            const particle = this.nextState[belowIndex];
+
+            if (particle.isEmpty) {
+                returnIndex = belowIndex;
+                break;
+            }
+        }
+
+        return returnIndex;
     }
 
-    setNextStatePoint(column, row, value) {
-        this.nextState[row * this.columns + column] = value;
+    generateInitialState() {
+        const middleCol = Math.floor(this.columns / 2);
+        const middleRow = Math.floor(this.rows / 2);
+
+        const state = new Array(this.columns * this.rows).fill(new Empty);
+
+        state[middleCol + ((middleRow - 2) * this.columns)] = new Sand(1);
+        state[middleCol + middleRow * this.columns] = new Sand(1);
+        state[middleCol - 1 + (middleRow * this.columns)] = new Sand(1);
+        state[middleCol + 1 + (middleRow * this.columns)] = new Sand(1);
+        state[middleCol + (middleRow - 1) * this.columns] = new Sand(1);
+        state[middleCol - 1 + ((middleRow - 1) * this.columns)] = new Sand(1);
+        state[middleCol + 1 + ((middleRow - 1) * this.columns)] = new Sand(1);
+        state[middleCol + ((middleRow + 1) * this.columns)] = new Sand(1);
+
+        return state;
     }
 
     generateNextState() {
+        for (let row = this.rows - 1; row >= 0; row--) {
+            // determine inner loop direction
+            const innerLoopDirection = Math.random() > 0.5;
 
-        for (let i = this.state.length -1; i >= 0; i--) {
-            if (!this.isEmpty(i)) {
+            for (let i = 0; i < this.columns; i++) {
+                const column = innerLoopDirection ? i : -i - 1 + this.columns;
+                const index = (row * this.columns) + column;
+                const isParticle = !this.isEmpty(index);
 
-                for (let j = this.gravityState[i]; j >= 0; j--) {
-                    this.determineNextPosition(i, j);
+                if (isParticle) {
+                    const nextIndex = this.findNextBelowIndex(index);
+                    const isSameIndex = index === nextIndex;
+
+                    if (isSameIndex) {
+                        this.setNextPosition(index);
+                    } else {
+                        this.increaseParticleVelocity(index, nextIndex);
+                    }
                 }
-
-
-
-
             }
         }
 
         this.state = this.nextState;
-        this.resetNext();
-    }
-
-    determineNextPosition(i, j) {
-        const current = this.state[i];
-        const belowIndex = this.getBelowIndex(i) + (j * this.columns);
-        const belowLeftIndex = belowIndex - 1;
-        const belowRightIndex = belowIndex + 1;
-
-        console.log({ belowIndex: belowIndex, leftIndex: belowLeftIndex, rightIndex: belowRightIndex });
-
-
-        const hasValidBelow = this.hasOpenBelow(belowIndex);
-        const hasValidLeft = this.hasOpenBelowLeft(belowLeftIndex);
-        const hasValidRight = this.hasOpenBelowRight(belowRightIndex);
-
-        console.log({ below: hasValidBelow, left: hasValidLeft, right: hasValidRight });
-
-        if (hasValidBelow) {
-            this.nextState[belowIndex] = current;
-        }
-        else if (hasValidLeft && hasValidRight) {
-            this.nextState[sample([belowLeftIndex, belowRightIndex])] = current;
-        }
-        else if (hasValidLeft) {
-            this.nextState[belowLeftIndex] = current;
-        }
-        else if (hasValidRight) {
-            this.nextState[belowRightIndex] = current;
-        }
-        else {
-            this.nextState[i] = current;
-        }
-    }
-
-    isEmpty(index) {
-        return this.state[index].isEmpty;
-    }
-
-    getCurrentColumn(index) {
-        return index - (this.getCurrentRow(index) * this.columns);
-    }
-
-    getCurrentRow(index) {
-        return Math.floor((index / this.columns));
+        this.resetNextState();
     }
 
     getBelowIndex(index) {
         return index + this.columns;
     }
 
-    hasOpenBelow(index) {
-        const isEmpty = this.nextState[index]?.isEmpty ?? false;
-        return isEmpty && index < this.state.length;
+    getCurrentColumn(index) {
+        return index % this.columns;
     }
 
-
-    hasOpenBelowLeft(index) {
-        const isEmpty = this.nextState[index]?.isEmpty ?? false;
-        return isEmpty && this.getCurrentColumn(index) !== 0;
+    getCurrentRow(index) {
+        return Math.floor((index / this.columns));
     }
 
-    hasOpenBelowRight(index) {
-        const isEmpty = this.nextState[index]?.isEmpty ?? false;
-        return isEmpty && this.getCurrentColumn(index) !== this.columns - 1;
-    }
-
-    getBlockX(index) {
+    getGridX(index) {
         return this.getCurrentColumn(index) * this.particleWidth;
     }
 
-    getBlockY(index) {
+    getGridY(index) {
         return this.getCurrentRow(index) * this.particleWidth;
+    }
+
+    getParticleVelocity(index) {
+        return this.state[index].velocity;
+    }
+
+    hasOpenBelowLeft(index) {
+        return this.nextState[index - 1]?.isEmpty && this.getCurrentColumn(index) !== 0;
+    }
+
+    hasOpenBelowRight(index) {
+        return this.nextState[index + 1]?.isEmpty && this.getCurrentColumn(index) !== this.columns - 1;
+    }
+
+    increaseParticleVelocity(index, nextIndex) {
+        const updatedParticle = this.state[index];
+        updatedParticle.velocity += 1;
+        this.nextState[nextIndex] = updatedParticle;
+    }
+
+    isEmpty(index) {
+        return this.state[index].isEmpty;
+    }
+
+    isInbounds(index) {
+        const column = this.getCurrentColumn(index);
+        const row = this.getCurrentRow(index);
+
+        const isColumnInBounds = column >= 0 && column < this.columns;
+        const isRowInBounds = row >= 0 && row < this.rows;
+
+        return isColumnInBounds && isRowInBounds;
+    }
+
+    resetNextState() {
+        this.nextState = new Array(this.columns * this.rows).fill(new Empty);
+    }
+
+    resetState() {
+        this.state = new Array(this.columns * this.rows).fill(new Empty);
+    }
+
+    setNext(index, particle) {
+        this.nextState[index] = particle;
+    }
+
+    setNextPosition(index) {
+        const particle = this.state[index];
+        particle.velocity = 1;
+
+        const belowIndex = this.getBelowIndex(index);
+        const belowLeftIndex = belowIndex - 1;
+        const belowRightIndex = belowIndex + 1;
+
+        const hasValidLeft = this.hasOpenBelowLeft(belowIndex);
+        const hasValidRight = this.hasOpenBelowRight(belowIndex);
+
+        if (hasValidLeft && hasValidRight) {
+            const direction = sample([belowLeftIndex, belowRightIndex])
+            this.setNext(direction, particle);
+        } else if (hasValidLeft) {
+            this.setNext(belowLeftIndex, particle);
+        } else if (hasValidRight) {
+            particle.velocity = 0;
+            this.setNext(belowRightIndex, particle);
+        } else {
+            particle.velocity = 0;
+            this.setNext(index, particle);
+        }
+    }
+
+    setStateParticle(column, row) {
+        this.state[(row * this.columns) + column] = new Sand(1);
     }
 }
--- public/js/classes/Particle.js
+++ public/js/classes/Particle.js
@@ -1,6 +1,7 @@
 class Particle {
-    constructor({ color, isEmpty }) {
+    constructor({ color, isEmpty, velocity = 0}) {
         this.color = color;
         this.isEmpty = isEmpty;
+        this.velocity = velocity
     }
 }
